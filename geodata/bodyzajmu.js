092d41857cd4dd3bb05f8f176e6cc08039a90e6c
bbscout <43731495+bbscout@users.noreply.github.com>
2022-08-22 12:04:15 +0200

    Add files via upload

---
+++ geodata/bodyzajmu.js
@@ -0,0 +1,25 @@
+var bodyzajmu = {
+    type: "FeatureCollection",
+    features: [{
+        type: "Feature",
+        properties: {
+            nadpis: "<span class='mod'>1. </span> ZŠ Komenského 17 - podkrovní učebny",
+            popis: "<p>V posledních 2 letech došlo na ZŠ Domažlice, Komenského 17 k realizaci projektu, který zlepšuje podmínky pro výuku budoucích generací.</p><p>Celá škola se stala bezbariérovou, budova 2. stupně se rozšířila o půdní vestavbu, došlo k rekonstrukci a k novému vybavení 15 odborných učeben a v těchto dnech se dokončuje stavba nové moderní počítačové sítě, která bude odpovídat současným standardům bezpečnosti a konektivity.</p><p>V neposlední řadě došlo k úpravě veřejného prostranství před hlavním vstupem do školy. V podpoře školství na všech úrovní hodláme pokračovat i do budoucna.</p>",
+            ikona: "zskom",
+            fotka: [{
+                src: "zskom_1",
+                autor: "Martin Ticháček"
+            },
+            {
+                src: "zskom_2",
+                autor: "Martin Ticháček"
+            }
+        ],
+            malaikona: "realizace"
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [12.9293672,49.4419947]
+        }
+    }]
+};
