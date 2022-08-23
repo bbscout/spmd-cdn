6014dd1e6974b72477690499462c1e4b19b24e9f
bbscout <43731495+bbscout@users.noreply.github.com>
2022-08-23 17:30:55 +0200

    Update bodyzajmu.js

--- geodata/bodyzajmu.js
+++ geodata/bodyzajmu.js
@@ -33,7 +33,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>2. </span> Rekonstrukce komunikace a Příhodova parku u pošty",
                 "popis": "<p>Kompletní rekonstrukcí včetně navazujícího parku Antonína Příhody prošla v roce 2019 ulice u pošty. V historickém centru tak na zásadní rekonstrukci čeká už jenom Kostelní ulice. Věříme, že se na ni dostane co nejdřív, máme to v plánu.</p>",
                 "ikona": "prihoda",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -50,7 +54,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>3. </span>Pivovar - pivnice, restaurace, vzdělávací centrum  a komunitní centrum",
                 "popis": "<p>V roce 2020 se podařilo úspěšně dokončit kulturní centrum v pivovaru! Jedná se o projekt, který přesahuje několik volebních období a který nebylo lehké dotáhnout do zdárného konce, ale podařilo se to na jedničku. Výstavy, knihovna, prostory pro ZUŠ, komunitní centrum, restaurace a v neposlední řadě pivnice s minipivovarem, kde se opět vaří Domažlické pivo.</p><p>Rekonstrukce získala řadu ocenění, mimo jiné Stavba roku Plzeňského kraje.</p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -67,7 +75,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>4. </span>Sportovní centrum – atletický tunel a tělocvična v ulici El. Krásnohorské",
                 "popis": "<p>Z bývalé ubytovny na střelnici se podařilo vybudovat sportovní centrum s krytou atletickou dráhou a sálem na karate. Centrum se otevřelo v zimě 2019. Krytou atletickou dráhu od té doby využívají během zimní přípravy i jiné oddíly než jen atleti. Z našeho pohledu se jedná o úspěšný projekt.</p>",
                 "ikona": "sportcentrum",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -84,7 +96,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>5. </span>Rekonstrukce plaveckého bazénu, ubytovny a nová posilovna",
                 "popis": "<p>Rekonstrukce bazénu. Zásadní investiční akce, která rozdělovala společnost. My máme svědomí čisté a jsme přesvědčeni o tom, že rekonstruovat starý bazén bylo správné rozhodnutí. Z původně plaveckého bazénu vzniklo moderní aquacentrum. Plavecký bazén, dětský bazén, vodní atrakce s tobogánem, wellness, bazén se slanou vodou, posilovna, bistro, bowling a ubytování, to vše pod jednou střechou.</p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -101,7 +117,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>6. </span>Demolice vojenských objektů na Čerchově a v jeho okolí",
                 "popis": "<p>Těsně před revolucí byla na vrcholu Čerchova postavena nová vojenská rota, za účelem „ochrany hranic“. S pádem železné opony se vrchol Čerchova stal opět přístupný veřejnosti, vojenské objekty zde ale zůstaly.</p><p>Dlouhodobé vyjednávání s ministerstvem vnitra přineslo ovoce. Městu se podařilo většinu budov získat do svého vlastnictví. V loňském roce pak došlo k demolici většiny vojenských objektů na vrcholu Čerchova. Demolice byla hrazena více jak z 90 % z dotací. Doporučujeme návštěvu Čerchova zařadit do letošních výletů. Pokud si netroufnete na pěší výšlap, nebo se vám nechce šlapat na kole, můžete využít prázdninový autobus, který vás vyveze nahoru.</p>",
                 "ikona": "cerchov",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -118,7 +138,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>7. </span>Nové řadové domy pro Domažlickou nemocnici Na Bábě",
                 "popis": "<p>Nalákat nové sestry a doktory do nemocnice, když máte za humny Německo není jen tak. I když Domažlickou nemocnici spravuje kraj a město tudíž na její provoz má minimální vliv, rozhodlo se vybudovat za vlastní náklady devět řadových domů, které následně byly předány do dlouhodobého pronájmu nemocnici, aby mohla lékařům nabízet odpovídající bydlení a podařilo se je tak udržet ve městě. V podpoře lékařů a sester město plánuje pokračovat i do budoucna.</p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -135,7 +159,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>8. </span>Nová školka",
                 "popis": "<p>V roce 2021 se slavnostně otevřela nová školka v Petrovické ulici. Ve školce jsou 3 samostatná oddělení. Obálka budovy splňuje pasivní standardy, vnitřní prostory mají řízené větrání, což zajišťuje zdravé vnitřní prostředí po celý den. Dále je v objektu využívaná šedá a dešťová voda. Školku stavěla místní firma „DSP“. Více jak 90 % nákladů bylo hrazeno z dotací Evropské unie.</p><p>Celkově jsme za 16 let vytvořili bezmála 200 nových míst ve školkách, všechny domažlické děti mají tak péči zajištěnou.</p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -152,7 +180,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>9. </span>Vodní nádrže na bývalém \"Cvičáku\"",
                 "popis": "<p>Zadržování vody v krajině je v poslední době velké téma. My se o to snažíme již dlouhou dobu. Revitalizujme stávající vodní plochy, budujeme nové drobné rybníky a tůně. Na cvičáku vznikla kaskáda rybníčků nad baseballovým hřištěm. Místo jako stvořené pro pejskaře nebo na rodinnou vycházku</p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -169,7 +201,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>10. </span>Nové informační centrum v Chodském hradu včetně veřejného WC",
                 "popis": "<p>Nové moderní „Íčko“ (infocentrum) se podařilo vybudovat v prostorách nevyužívané restaurace v Chodském hradě. Íčko stojí za návštěvu i Domažlickým! V nabídce jsou různé knihy o Chodsku, dárkové předměty a pořídíte tu lístky na všechny kulturní akce ve městě. Součástí infocentra jsou i veřejné toalety.</p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -186,7 +222,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>11. </span>Stezka pro pěší a cyklisty -  cyklostezka podél hřbitova",
                 "popis": "<p></p>",
                 "ikona": "cyklo",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -203,7 +243,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>12. </span>Nový památník osvobození pod Chodským hradem",
                 "popis": "<p>V loňském roce byl pod Chodským hradem odhalen nový památník osvobození. První myšlenka na nový památník vzešla od rodin válečných veteránů, kteří osvobozovali Domažlice na konci druhé světové války. Letošních oslav osvobození se dokonce osobně zúčastnil veterán Herman Geist. Děkujeme, nezapomeneme!</p>",
                 "ikona": "pamatnik",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -220,7 +264,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>13. </span>Dům se zvláštním režimem – přístavba Benešova ul.",
                 "popis": "<p></p>",
                 "ikona": "ostatni",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
@@ -237,7 +285,11 @@ var bodyzajmu = {
                 "nadpis": "<span class='mod'>14. </span>Nové lyžařské trasy kolem Čerchova",
                 "popis": "<p></p>",
                 "ikona": "trasy",
-                "fotka": {},
+                "fotka": [{
+                    "src": "",
+                    "autor": ""
+                }
+            ],
                 "malaikona": "realizace"
             },
             "geometry": {
