5b01253ffd86428c6bca4700e09a49a20341b61d
bbscout <43731495+bbscout@users.noreply.github.com>
2022-08-23 17:26:54 +0200

    Update bodyzajmu.js

--- geodata/bodyzajmu.js
+++ geodata/bodyzajmu.js
@@ -1,37 +1,252 @@
 var bodyzajmu = {
-    type: "FeatureCollection",
-    features: [{
-        type: "Feature",
-        properties: {
-            nadpis: "<span class='mod'>1. </span> ZŠ Komenského 17 - podkrovní učebny",
-            popis: "<p>V posledních 2 letech došlo na ZŠ Domažlice, Komenského 17 k realizaci projektu, který zlepšuje podmínky pro výuku budoucích generací.</p><p>Celá škola se stala bezbariérovou, budova 2. stupně se rozšířila o půdní vestavbu, došlo k rekonstrukci a k novému vybavení 15 odborných učeben a v těchto dnech se dokončuje stavba nové moderní počítačové sítě, která bude odpovídat současným standardům bezpečnosti a konektivity.</p><p>V neposlední řadě došlo k úpravě veřejného prostranství před hlavním vstupem do školy. V podpoře školství na všech úrovní hodláme pokračovat i do budoucna.</p>",
-            ikona: "zskom",
-            fotka: [{
-                src: "zskom_1",
-                autor: "Martin Ticháček"
+    "type": "FeatureCollection",
+    "name": "geodata",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>1. </span>ZŠ Komenského 17 - podkrovní učebny",
+                "popis": "<p>V posledních 2 letech došlo na ZŠ Domažlice, Komenského 17 k realizaci projektu, který zlepšuje podmínky pro výuku budoucích generací.</p><p>Celá škola se stala bezbariérovou, budova 2. stupně se rozšířila o půdní vestavbu, došlo k rekonstrukci a k novému vybavení 15 odborných učeben a v těchto dnech se dokončuje stavba nové moderní počítačové sítě, která bude odpovídat současným standardům bezpečnosti a konektivity.</p><p>V neposlední řadě došlo k úpravě veřejného prostranství před hlavním vstupem do školy. V podpoře školství na všech úrovní hodláme pokračovat i do budoucna.</p>",
+                "ikona": "zskom",
+                "fotka": [{
+                    "src": "zskom_1",
+                    "autor": "Martin Ticháček"
+                },
+                {
+                    "src": "zskom_2",
+                    "autor": "Martin Ticháček"
+                }
+            ],
+                "malaikona": "realizace"
             },
-            {
-                src: "zskom_2",
-                autor: "Martin Ticháček"
-            },
-            {
-                src: "zskom_3",
-                autor: "Martin Ticháček"
-            },
-            {
-                src: "zskom_4",
-                autor: "Martin Ticháček"
-            },
-            {
-                src: "zskom_5",
-                autor: "Martin Ticháček"
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.9293672,
+                    49.4419947
+                ]
             }
-        ],
-            malaikona: "realizace"
         },
-        geometry: {
-            type: "Point",
-            coordinates: [12.9293672,49.4419947]
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>2. </span> Rekonstrukce komunikace a Příhodova parku u pošty",
+                "popis": "<p>Kompletní rekonstrukcí včetně navazujícího parku Antonína Příhody prošla v roce 2019 ulice u pošty. V historickém centru tak na zásadní rekonstrukci čeká už jenom Kostelní ulice. Věříme, že se na ni dostane co nejdřív, máme to v plánu.</p>",
+                "ikona": "prihoda",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.926223638326814,
+                    49.440279399709979
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>3. </span>Pivovar - pivnice, restaurace, vzdělávací centrum  a komunitní centrum",
+                "popis": "<p>V roce 2020 se podařilo úspěšně dokončit kulturní centrum v pivovaru! Jedná se o projekt, který přesahuje několik volebních období a který nebylo lehké dotáhnout do zdárného konce, ale podařilo se to na jedničku. Výstavy, knihovna, prostory pro ZUŠ, komunitní centrum, restaurace a v neposlední řadě pivnice s minipivovarem, kde se opět vaří Domažlické pivo.</p><p>Rekonstrukce získala řadu ocenění, mimo jiné Stavba roku Plzeňského kraje.</p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.927266101525108,
+                    49.441782579011011
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>4. </span>Sportovní centrum – atletický tunel a tělocvična v ulici El. Krásnohorské",
+                "popis": "<p>Z bývalé ubytovny na střelnici se podařilo vybudovat sportovní centrum s krytou atletickou dráhou a sálem na karate. Centrum se otevřelo v zimě 2019. Krytou atletickou dráhu od té doby využívají během zimní přípravy i jiné oddíly než jen atleti. Z našeho pohledu se jedná o úspěšný projekt.</p>",
+                "ikona": "sportcentrum",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.921286502870467,
+                    49.43828529879783
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>5. </span>Rekonstrukce plaveckého bazénu, ubytovny a nová posilovna",
+                "popis": "<p>Rekonstrukce bazénu. Zásadní investiční akce, která rozdělovala společnost. My máme svědomí čisté a jsme přesvědčeni o tom, že rekonstruovat starý bazén bylo správné rozhodnutí. Z původně plaveckého bazénu vzniklo moderní aquacentrum. Plavecký bazén, dětský bazén, vodní atrakce s tobogánem, wellness, bazén se slanou vodou, posilovna, bistro, bowling a ubytování, to vše pod jednou střechou.</p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.9168538650618,
+                    49.436674805650718
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>6. </span>Demolice vojenských objektů na Čerchově a v jeho okolí",
+                "popis": "<p>Těsně před revolucí byla na vrcholu Čerchova postavena nová vojenská rota, za účelem „ochrany hranic“. S pádem železné opony se vrchol Čerchova stal opět přístupný veřejnosti, vojenské objekty zde ale zůstaly.</p><p>Dlouhodobé vyjednávání s ministerstvem vnitra přineslo ovoce. Městu se podařilo většinu budov získat do svého vlastnictví. V loňském roce pak došlo k demolici většiny vojenských objektů na vrcholu Čerchova. Demolice byla hrazena více jak z 90 % z dotací. Doporučujeme návštěvu Čerchova zařadit do letošních výletů. Pokud si netroufnete na pěší výšlap, nebo se vám nechce šlapat na kole, můžete využít prázdninový autobus, který vás vyveze nahoru.</p>",
+                "ikona": "cerchov",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.784893642512824,
+                    49.382985643931747
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>7. </span>Nové řadové domy pro Domažlickou nemocnici Na Bábě",
+                "popis": "<p>Nalákat nové sestry a doktory do nemocnice, když máte za humny Německo není jen tak. I když Domažlickou nemocnici spravuje kraj a město tudíž na její provoz má minimální vliv, rozhodlo se vybudovat za vlastní náklady devět řadových domů, které následně byly předány do dlouhodobého pronájmu nemocnici, aby mohla lékařům nabízet odpovídající bydlení a podařilo se je tak udržet ve městě. V podpoře lékařů a sester město plánuje pokračovat i do budoucna.</p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.906925053862858,
+                    49.441120326069743
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>8. </span>Nová školka",
+                "popis": "<p>V roce 2021 se slavnostně otevřela nová školka v Petrovické ulici. Ve školce jsou 3 samostatná oddělení. Obálka budovy splňuje pasivní standardy, vnitřní prostory mají řízené větrání, což zajišťuje zdravé vnitřní prostředí po celý den. Dále je v objektu využívaná šedá a dešťová voda. Školku stavěla místní firma „DSP“. Více jak 90 % nákladů bylo hrazeno z dotací Evropské unie.</p><p>Celkově jsme za 16 let vytvořili bezmála 200 nových míst ve školkách, všechny domažlické děti mají tak péči zajištěnou.</p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.912329500408108,
+                    49.442983746005424
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>9. </span>Vodní nádrže na bývalém \"Cvičáku\"",
+                "popis": "<p>Zadržování vody v krajině je v poslední době velké téma. My se o to snažíme již dlouhou dobu. Revitalizujme stávající vodní plochy, budujeme nové drobné rybníky a tůně. Na cvičáku vznikla kaskáda rybníčků nad baseballovým hřištěm. Místo jako stvořené pro pejskaře nebo na rodinnou vycházku</p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.904938465255098,
+                    49.42760572635774
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>10. </span>Nové informační centrum v Chodském hradu včetně veřejného WC",
+                "popis": "<p>Nové moderní „Íčko“ (infocentrum) se podařilo vybudovat v prostorách nevyužívané restaurace v Chodském hradě. Íčko stojí za návštěvu i Domažlickým! V nabídce jsou různé knihy o Chodsku, dárkové předměty a pořídíte tu lístky na všechny kulturní akce ve městě. Součástí infocentra jsou i veřejné toalety.</p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.92757433677917,
+                    49.439108526786889
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>11. </span>Stezka pro pěší a cyklisty -  cyklostezka podél hřbitova",
+                "popis": "<p></p>",
+                "ikona": "cyklo",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.943479386069678,
+                    49.441563082779588
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>12. </span>Nový památník osvobození pod Chodským hradem",
+                "popis": "<p>V loňském roce byl pod Chodským hradem odhalen nový památník osvobození. První myšlenka na nový památník vzešla od rodin válečných veteránů, kteří osvobozovali Domažlice na konci druhé světové války. Letošních oslav osvobození se dokonce osobně zúčastnil veterán Herman Geist. Děkujeme, nezapomeneme!</p>",
+                "ikona": "pamatnik",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.928201164432505,
+                    49.438611152573266
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>13. </span>Dům se zvláštním režimem – přístavba Benešova ul.",
+                "popis": "<p></p>",
+                "ikona": "ostatni",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.925174674367165,
+                    49.44273658365131
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nadpis": "<span class='mod'>14. </span>Nové lyžařské trasy kolem Čerchova",
+                "popis": "<p></p>",
+                "ikona": "trasy",
+                "fotka": {},
+                "malaikona": "realizace"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    12.785595173830883,
+                    49.375466753072864
+                ]
+            }
         }
-    }]
+    ]
 };
